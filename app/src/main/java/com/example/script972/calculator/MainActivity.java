4dca89ee72c108c3a84030dcf747767f6516bb8a
script972 <script972@gmail.com>
2017-05-08 19:07:05 +0300

    remove rect redius

--- app/src/main/java/com/example/script972/calculator/MainActivity.java
+++ app/src/main/java/com/example/script972/calculator/MainActivity.java
@@ -2,19 +2,14 @@ package com.example.script972.calculator;
 
 import java.util.EnumMap;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.script972.calculator.OperationType;
-import com.example.script972.calculator.Operation;
-import com.example.script972.calculator.R;
-import com.example.script972.calculator.Store;
 
 public class MainActivity extends AppCompatActivity {
 
